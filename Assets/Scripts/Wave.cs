6619e71b60f4ee7471e7436037215aa94db4ed92
Becher <mbelkhenchir@gmail.com>
2022-10-02 15:29:27 +0200

    Sprites for sand and wave

--- Assets/Scripts/Wave.cs
+++ Assets/Scripts/Wave.cs
@@ -1,12 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Mirror;
+using Unity.Mathematics;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Wave : MonoBehaviour
 {
     private float m_timer;
     private Animator m_animator;
+    [SerializeField] private SpriteRenderer m_waveLine;
+    [SerializeField] private SpriteRenderer m_waveLine2;
+    [SerializeField] private SpriteRenderer m_wave;
+    [SerializeField] private SpriteRenderer m_wetSand;
+
+    [Serializable] struct WaveSpriteSet
+    {
+        public Sprite m_waveLine;
+        public Sprite m_wave;
+        public Sprite m_wetSand;
+    }
+
+    [SerializeField] private List<WaveSpriteSet> m_sprites;
     
     public struct WaveMessage : NetworkMessage
     {}
@@ -19,14 +35,24 @@ public class Wave : MonoBehaviour
 
     public void SetupClient()
     {
-        Debug.Log("Setup Wave");
         m_animator = GetComponent<Animator>();
-        NetworkClient.RegisterHandler<WaveMessage>(OnScore);
+        NetworkClient.RegisterHandler<WaveMessage>(OnWave);
     }
 
-    public void OnScore(WaveMessage msg)
+    public void OnWave(WaveMessage msg)
     {
-        Debug.Log("OnWave");
+        if (m_sprites.Count > 0)
+        {
+            int i = (int)math.floor(Random.value * m_sprites.Count);
+            Debug.Log(i);
+            m_wave.sprite = m_sprites[i].m_wave;
+            m_waveLine.sprite = m_sprites[i].m_waveLine;
+            m_waveLine2.sprite = m_sprites[i].m_waveLine;
+            m_wetSand.sprite = m_sprites[i].m_wetSand;
+        }
+
+        transform.localScale = new Vector3(-transform.localScale.x ,transform.localScale.y ,transform.localScale.z);
+        
         m_animator.SetTrigger("SendWave");
     }
 
